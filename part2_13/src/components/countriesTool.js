b79c7ed0174c40084412bbcf36a1b548a74be48d
Arr170 <kuprin.ars@gmail.com>
2022-12-13 22:51:39 +0100

    git commit

--- part2_13/src/components/countriesTool.js
+++ part2_13/src/components/countriesTool.js
@@ -1,20 +1,15 @@
 
-const ShowOneCountry = ({toShow}) => {
-    console.log(toShow)
+///
+function ShowOneCountry (toShow) {
+    console.log('show one country', toShow)
     return(
         <div>
-        <h2>{toShow[0].name.official}</h2>
-        <p>capital: {toShow[0].capital}</p> 
-        <p>area: {toShow[0].area}</p>
-        <p>languages:</p>
-        {Object.values(toShow[0].languages).map(lang => <li key = {lang}>{lang} </li>)}
-        <p>
-        <img scr = {toShow[0].flags.png} alt = "flag" width = "100" height="100"/>
-        </p>
-       </div>   
+            <ShowCountries toShow = {toShow}/>
+        </div>
     )
 
 }
+///
 const ShowCountries = ({toShow}) => {
 console.log('to show:', toShow)
 if(toShow.length === 1)
@@ -28,10 +23,7 @@ if(toShow.length === 1)
          <p>area: {toShow[0].area}</p>
          <p>languages:</p>
          {Object.values(toShow[0].languages).map(lang => <li key = {lang}>{lang} </li>)}
-         <p>
-         <img scr = {toShow[0].flags.png} alt = "flag" width = "100" height="100"/>
-         </p>
-         
+         <img scr = {toShow[0].flags.png} alt = "flag"/>
         </div>
     )
 }
@@ -48,17 +40,12 @@ else
 {
     return(
         <div>
-            {toShow.map(country => {
-                <div>
-            <li key = {country.name.official}>{country.name.official}</li>
-            <button onClick = {ShowOneCountry(country)}>show</button>
-                </div>
-                })}
+            {toShow.map(country => <div><p key = {country.name.official}>{country.name.official}      <button onClick = {ShowOneCountry(country.name.official)}>show</button></p></div>)}
         </div>
     )
 }
 }
-
+///
 function FindCountries(toFind, whereLook) {
 console.log("toFind, whereLook", toFind, whereLook)
 let found
