eb7e1685907ece9c0c95f58e3da58802c9661fc8
Arr170 <kuprin.ars@gmail.com>
2022-12-14 18:22:51 +0100

    git commit

--- part2_13/src/components/countriesTool.js
+++ part2_13/src/components/countriesTool.js
@@ -1,57 +1,74 @@
-
+import {useState} from 'react'
+import React from 'react'
 ///
-function ShowOneCountry (toShow) {
-    console.log('show one country', toShow)
+const ShowOneCountry  = ({toShow, bool}) => {
+    console.log('show one country', toShow, bool)
+    if(bool)
+    {
     return(
         <div>
-            <ShowCountries toShow = {toShow}/>
+            here we go
         </div>
     )
-
+    }
+    else
+    {
+        return(<></>)
+    }
 }
 ///
 const ShowCountries = ({toShow}) => {
-console.log('to show:', toShow)
-if(toShow.length === 1)
-{
-    console.log('showing one country', toShow)
-    console.log(toShow[0].flags.png)
-    return(
-        <div>
-         <h2>{toShow[0].name.official}</h2>
-         <p>capital: {toShow[0].capital}</p> 
-         <p>area: {toShow[0].area}</p>
-         <p>languages:</p>
-         {Object.values(toShow[0].languages).map(lang => <li key = {lang}>{lang} </li>)}
-         <img scr = {toShow[0].flags.png} alt = "flag"/>
-        </div>
-    )
-}
-else if(toShow.length > 10)
-{
-    console.log('showing more then 10')
-    return(
-        <div>
-            Too many matches, specify another filter
-        </div>
-    )
-}
-else
-{
-    return(
-        <div>
-            {toShow.map(country => <div><p key = {country.name.official}>{country.name.official}      <button onClick = {ShowOneCountry(country.name.official)}>show</button></p></div>)}
-        </div>
-    )
-}
+    const [showOne, setShowOne] = useState(false)
+
+    const turnShow = (event) => {
+        // event.preventDefault()
+        console.log('button pressed')
+        setShowOne(true)
+    }
+
+    //console.log('to show:', toShow)
+
+    if(toShow.length === 1)
+    {
+        console.log('showing one country', toShow)
+        console.log(toShow[0].flags.png)
+        return(
+            <div>
+            <h2>{toShow[0].name.official}</h2>
+            <p>capital: {toShow[0].capital}</p> 
+            <p>area: {toShow[0].area}</p>
+            <p>languages:</p>
+            {Object.values(toShow[0].languages).map(lang => <li key = {lang}>{lang} </li>)}
+            <img src = {toShow[0].flags.png}/>
+            </div>
+        )
+    }
+    else if(toShow.length > 10)
+    {
+        console.log('showing more then 10')
+        return(
+            <div>
+                Too many matches, specify another filter
+            </div>
+        )
+    }
+    else
+    {
+        return(
+            <div>
+                {toShow.map(country => <div><p key = {country.name.official}>{country.name.official}      <button onClick = {() =>turnShow()}>show</button></p><ShowOneCountry toShow = {country} bool = {showOne}/></div>)}
+                
+            </div>
+        )
+    }
 }
 ///
 function FindCountries(toFind, whereLook) {
-console.log("toFind, whereLook", toFind, whereLook)
-let found
-found = whereLook.filter(country => country.name.official.includes(toFind))
-console.log('i found:', found)
-return(found)
+    console.log("toFind, whereLook", toFind, whereLook)
+    let found
+    found = whereLook.filter(country => country.name.official.includes(toFind))
+    console.log('i found:', found)
+    return(found)
 }
 
 export  {FindCountries, ShowCountries}
